3e641213ead13cbbe1eda6cf67f4a5d84234a6a5
lekeee <jaleksa388@gmail.com>
2025-02-23 16:54:16 +0100

    hamburger menu

--- app/components/scroll-header.tsx
+++ app/components/scroll-header.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { useState } from "react";
 import { useScroll } from "../hooks/useScroll";
 import Header from "./header";
 
 export default function ScrollHeader() {
   const isScrolled = useScroll();
+  const [isOpen, setIsOpen] = useState(false);
 
-  return <Header isScrolled={isScrolled} />;
+  return (
+    <Header
+      isScrolled={isScrolled}
+      isOpen={isOpen}
+      toggleMenu={() => setIsOpen(!isOpen)}
+    />
+  );
 }
