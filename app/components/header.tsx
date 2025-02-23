948ecedb769dd8fc19e515fbdadc720c7960371e
lekeee <jaleksa388@gmail.com>
2025-02-23 20:53:01 +0100

    header responsive done

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -11,7 +11,7 @@ type Props = {
 export default function Header({ isScrolled, isOpen, toggleMenu }: Props) {
   return (
     <header
-      className={`container sticky top-0 z-50 py-2 transition-all duration-500 ${
+      className={`w-full p-8 sticky top-0 z-50 py-2 transition-all duration-500 text-x ${
         isScrolled ? "bg-white shadow-lg" : "bg-transparent"
       } `}
     >
@@ -26,19 +26,41 @@ export default function Header({ isScrolled, isOpen, toggleMenu }: Props) {
           />
         </Link>
 
-        <div className="hidden md:flex gap-10">
-          <Link href="/">Pocetna</Link>
-          <Link href="/">Ponude</Link>
-          <Link href="/destinations">Destinacije</Link>
-          <Link href="#">O nama</Link>
-        </div>
+        <div className="flex justify-center items-center gap-20">
+          <div className="hidden md:flex gap-1">
+            <Link
+              className="rounded-3xl px-5 py-2 hover:bg-border transition-all duration-300 ease-in-out"
+              href="/"
+            >
+              Pocetna
+            </Link>
+            <Link
+              className="rounded-3xl px-5 py-2 hover:bg-border transition-all duration-300 ease-in-out"
+              href="/"
+            >
+              Ponude
+            </Link>
+            <Link
+              className="rounded-3xl px-5 py-2 hover:bg-border transition-all duration-300 ease-in-out"
+              href="/destinations"
+            >
+              Destinacije
+            </Link>
+            <Link
+              className="rounded-3xl px-5 py-2 hover:bg-border transition-all duration-300 ease-in-out"
+              href="#"
+            >
+              O nama
+            </Link>
+          </div>
 
-        <Link
-          href="#"
-          className="hidden md:flex bg-main rounded-full py-3 px-5 text-white"
-        >
-          Rezervisi putovanje
-        </Link>
+          <Link
+            href="#"
+            className="hidden md:flex bg-main rounded-full py-3 px-5 text-white  hover:bg-title transition-all duration-300 ease-in-out"
+          >
+            Rezervisi putovanje
+          </Link>
+        </div>
 
         <HamburgerMenu isOpen={isOpen} toggleMenu={toggleMenu} />
       </nav>
