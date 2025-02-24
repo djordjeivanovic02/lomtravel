41b9b204e7cce2e104f3e1c8631cb6c989acf5cc
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2025-02-24 16:59:21 +0100

    Hero Section

---
+++ app/components/heroSearchItem.tsx
@@ -0,0 +1,49 @@
+import CustomIcon from "./icon";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "@heroui/react";
+
+type Props = {
+  icon: string;
+  title: string;
+  desc: string;
+  items: string[];
+  border?: boolean
+};
+
+export default function HeroSectionItem({ icon, title, desc, items, border = true}: Props) {
+  return (
+    <Dropdown className="w-72 md:w-64 rounded-md ml-16 mt-2">
+      <DropdownTrigger>
+        <Button variant="bordered" className={`outline-none w-full md:w-auto py-2 md:py-0 px-5 md:px-0 md:border-b md:border-none ${border ? 'border-b md:border-none pb-3 md:pb-0': ''}`}>
+          <div className="flex items-center gap-3">
+            <div className="px-3 py-3 rounded-full border flex items-center justify-center">
+              <CustomIcon name={icon} size={24} color="black" />
+            </div>
+            <div className="flex flex-col gap-0 items-start">
+              <p className="roboto">{title}</p>
+              <p className="roboto text-lightText">{desc}</p>
+            </div>
+          </div>
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu
+        aria-label="Static Actions"
+        className="w-full bg-white outline-none rounded-md border my-0"
+      >
+        {items.map((element) => (
+          <DropdownItem
+            key={element}
+            className="w-full border-b roboto text-base py-4"
+          >
+            <p className="px-6">{element}</p>
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${dmSans.variable} antialiased`}>
         <ScrollHeader />
-        {children}
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
