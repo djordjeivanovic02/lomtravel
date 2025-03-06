7d1d67e122452869c18c0edcdafa16b6b0a229ce
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2025-03-06 21:57:32 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
             href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
           />
         </head>
-        <body className={`${dmSans.variable} antialiased overflow-x-hidden`}>
+        <body className={`${dmSans.variable} antialiased overflow-x-hidden w-screen`}>
           <NextTopLoader color="#f38255" showSpinner={false} />
 
           <Provider>
